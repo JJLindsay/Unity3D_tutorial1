11403a200b5aa8776d4675f4e83ec0957ef81d7f
unknown <jlindsay1@ggc.edu>
2015-05-31 21:25:04 -0400

    added Space Shooter... IN PROGRESS. Ship moves and tilts and there is a background.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Unity3D_tutorial_01
 This is my first attempt at learning Unity3D.<br/>
-In this game the player collects cubes and after collecting all 12 a win message appears.
 
 <br/>The Executable is for Windows x86 and located in the Builds folder.
 
-
+<ol>
+	<li><b>Roll A Ball</b> This game allows the user to roll a ball and collect 12 game pieces to receive a "You Win!" message.</li>
+	<li><b>Space Shooter... IN PROGRESS</b> This game allows the user pilot a ship and shoot asteroids in space</li>
+</ol>
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public Boundary boundary; //references created class
+	public float tilt;  //tilt the ship on turn
+
+	//called auto by unity before each Fixed Physics Step
+	void FixedUpdate()
+	{
+		//allows the player to move the player obj
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//x,y,z  <-- check your map to see moving in x and Z not y
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3 
+			(
+				Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+			 	0.0f , 
+				Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			 );
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+}
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
